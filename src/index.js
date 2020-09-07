576c51e575b5e75e9e5834f4458078a56d1ec47d
glennpacker <glenn.packer@branthill.co.uk>
2020-09-07 22:13:00 +0100

    fix typo in filename

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import ErrorBoundary from './ErrorBoundary ';
+import ErrorBoundary from './ErrorBoundary';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
