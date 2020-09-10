bbe3d0f771a8d08c3146fef3acb48cbf65e6d564
glennpacker <glenn.packer@branthill.co.uk>
2020-09-10 19:17:25 +0100

    loading html should use classname and not class

--- src/Spinner.jsx
+++ src/Spinner.jsx
@@ -5,13 +5,13 @@ export default function Spinner() {
     <>
     <div className="spinner">
         <div id="inTurnFadingTextG">
-            <div id="inTurnFadingTextG_1" class="inTurnFadingTextG">L</div>
-            <div id="inTurnFadingTextG_2" class="inTurnFadingTextG">o</div>
-            <div id="inTurnFadingTextG_3" class="inTurnFadingTextG">a</div>
-            <div id="inTurnFadingTextG_4" class="inTurnFadingTextG">d</div>
-            <div id="inTurnFadingTextG_5" class="inTurnFadingTextG">i</div>
-            <div id="inTurnFadingTextG_6" class="inTurnFadingTextG">n</div>
-            <div id="inTurnFadingTextG_7" class="inTurnFadingTextG">g</div>
+            <div id="inTurnFadingTextG_1" className="inTurnFadingTextG">L</div>
+            <div id="inTurnFadingTextG_2" className="inTurnFadingTextG">o</div>
+            <div id="inTurnFadingTextG_3" className="inTurnFadingTextG">a</div>
+            <div id="inTurnFadingTextG_4" className="inTurnFadingTextG">d</div>
+            <div id="inTurnFadingTextG_5" className="inTurnFadingTextG">i</div>
+            <div id="inTurnFadingTextG_6" className="inTurnFadingTextG">n</div>
+            <div id="inTurnFadingTextG_7" className="inTurnFadingTextG">g</div>
         </div>
     </div>
     </>
