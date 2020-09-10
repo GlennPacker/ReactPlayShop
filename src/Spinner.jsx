66c487b593555ba0958e77b875c43ea466cc772f
glennpacker <glenn.packer@branthill.co.uk>
2020-09-10 18:48:43 +0100

    nice loading spinner

--- src/Spinner.jsx
+++ src/Spinner.jsx
@@ -2,8 +2,20 @@ import React from "react";
 
 export default function Spinner() {
   return (
-    <div className="lds-container">
-      <div className="lds-dual-ring"></div>
+    <>
+    <div className="spinner">
+        <div id="inTurnFadingTextG">
+            <div id="inTurnFadingTextG_1" class="inTurnFadingTextG">L</div>
+            <div id="inTurnFadingTextG_2" class="inTurnFadingTextG">o</div>
+            <div id="inTurnFadingTextG_3" class="inTurnFadingTextG">a</div>
+            <div id="inTurnFadingTextG_4" class="inTurnFadingTextG">d</div>
+            <div id="inTurnFadingTextG_5" class="inTurnFadingTextG">i</div>
+            <div id="inTurnFadingTextG_6" class="inTurnFadingTextG">n</div>
+            <div id="inTurnFadingTextG_7" class="inTurnFadingTextG">g</div>
+        </div>
     </div>
+    </>
   );
 }
+
+
