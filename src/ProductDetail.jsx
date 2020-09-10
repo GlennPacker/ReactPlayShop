441bed7653be5a07585d1f38c2026a8de67e0823
glennpacker <glenn.packer@branthill.co.uk>
2020-09-10 18:50:33 +0100

    product details enhancements

--- src/ProductDetail.jsx
+++ src/ProductDetail.jsx
@@ -1,19 +1,28 @@
 import React from "react";
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import useFetch from './services/useFetch';
 import Spinner from './Spinner';
+import PageNotFound from './PageNotFound';
 
 export default function ProductDetail() {
   const { id } = useParams();
+  const navigate = useNavigate();
   const { data: product, loading, error } = useFetch(`products/${ id }`)
 
-  if (loading) return <Spinner />
-  if (error) throw error;
+  if (loading || !product) return <Spinner />
+  if (!product) return <PageNotFound />
+  // if (error) throw error;
+
   return (
     <div id="detail">
       <h1>{product.name}</h1>
       <p>{product.description}</p>
       <p id="price">${product.price}</p>
+      <p>
+        <button
+            className="btn btn-primary"
+            onClick={() => navigate("/cart") } >Add To Cart</button>
+      </p>
       <img src={`/images/${product.image}`} alt={product.category} />
     </div>
   );
